071da6f708fb4b3316ed8868ecdcabe79edcaab8
Somnath Shirkule <shirkule.somnath@gmail.com>
2020-12-08 13:38:03 +0530

    Added Author Comment
    Added Author Comment in all files

--- ChatServerInterface.java
+++ ChatServerInterface.java
@@ -2,6 +2,7 @@ package chat;
 import java.rmi.*;
 public interface ChatServerInterface extends Remote
 {
+	//Author: Somnath Shirkule
 	public void login(String name,ChatClientInterface ref) throws RemoteException;
 	public void takemsg(String msg) throws RemoteException;
 	public void logout(String name) throws RemoteException;
